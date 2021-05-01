dc18454ae87af80bfaf51cff4c5f9ca41758f7d7
volgarfun <83359771+volgarfun@users.noreply.github.com>
2021-05-01 17:36:52 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'NzExODY4ODkzOTQ5NDYwNTQw.XsJRkg.BmEg_0aKfhXgmksvsqaNRt7kF88',
+        prefix: '²',
+        activity: 'Watching',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
